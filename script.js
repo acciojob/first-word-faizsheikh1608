4001325d9fe2a8f8d19607071e31ad5902e52623
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-06-06 12:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let index = 0
+	while(S[index] == " "){
+		index++
+	} 
+	let str = ''
+	while(s[index] != " "){
+		str += s[index]
+	}
+	return str
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
